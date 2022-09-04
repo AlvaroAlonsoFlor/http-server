d8b01922cbba6e3db1fcb8eb2966d2f37e8c9e28
Alvaro <alvaroalonsoflor@gmail.com>
2022-09-04 21:24:32 +0100

    refactor: move routes outside the start method

--- index.ts
+++ index.ts
@@ -1,7 +1,49 @@
-import http from 'http'
+import http, { IncomingMessage, ServerResponse } from 'http'
 import createApp from './lib/app'
+import { Route } from './lib/route'
 
 const app = createApp()
+
+const routes: Array<Route> = [{
+    url: '/hello',
+    method: 'GET',
+    requestHandler: (req: IncomingMessage, res: ServerResponse ) => {
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify({
+            'message': "Hello from the routes"
+        }))
+    }
+},
+{
+    url: '/user/1',
+    method: 'GET',
+    requestHandler: (req: IncomingMessage, res: ServerResponse ) => {
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify({
+            'id': '1234',
+            'name': 'John',
+            'surname': 'Smith'
+        }))
+    }
+}]
+
+const secondRouteBatch = [{
+    url: '/admin/1',
+    method: 'GET',
+    requestHandler: (req: IncomingMessage, res: ServerResponse ) => {
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify({
+            'id': '1625',
+            'name': 'Jane',
+            'surname': 'Smith',
+            'permissions': '777'
+        }))
+    }
+}]
+
+
+app.addRoutes(routes)
+app.addRoutes(secondRouteBatch)
 
 app.start()
 
--- lib/app.ts
+++ lib/app.ts
@@ -1,51 +1,36 @@
-import http, { IncomingMessage, RequestListener, Server, ServerResponse } from 'http'
+import http, { IncomingMessage, ServerResponse } from 'http'
 import { Route } from './route'
 
 class App {
 
     server
-    routes
+    routes: Array<Route>
     port
 
     constructor(routes: Array<Route>, port: Number) {
         this.server = http.createServer()
-        this.routes = routes
+        this.routes = []
         this.port = port
+    }
+
+    addRoutes(routes: Array<Route>) {
+        this.routes.concat(routes)
     }
 
     start() {
         
-        const routes: Array<Route> = [{
-            url: '/hello',
-            method: 'GET',
-            requestHandler: (req: IncomingMessage, res: ServerResponse ) => {
-                res.setHeader('Content-Type', 'application/json')
-                res.end(JSON.stringify({
-                    'message': "Hello from the routes"
-                }))
-            }
-        },
-        {
-            url: '/user/1',
-            method: 'GET',
-            requestHandler: (req: IncomingMessage, res: ServerResponse ) => {
-                res.setHeader('Content-Type', 'application/json')
-                res.end(JSON.stringify({
-                    'id': '1234',
-                    'name': 'John',
-                    'surname': 'Smith'
-                }))
-            }
-        }]
-
 
         this.server.on('request', (req: IncomingMessage, res: ServerResponse) => {
+            console.log(`Number of connections: ${this.server.connections}`)
             console.log(`Request: Date [${Date.now()}], URL [${req.url}], METHOD [${req.method}] `)
-            routes.map((route) => {
+            this.routes.map((route) => {
+                console.log(route.url)
                 if (route.url === req.url && route.method === req.method) {
                     route.requestHandler(req, res)
                 }
             })
+
+            
         })
 
         this.server.listen(this.port, () => {

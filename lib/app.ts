77773a51b4276d9a8ead882d95785519e9b21fed
Alvaro <alvaroalonsoflor@gmail.com>
2023-10-28 14:46:44 +0100

    fix: lint issues in app.ts

--- lib/app.ts
+++ lib/app.ts
@@ -1,63 +1,60 @@
-import http, { IncomingMessage, ServerResponse } from 'node:http'
-import { Route } from './route'
+import http, { type IncomingMessage, type ServerResponse } from 'node:http'
+import { type Route } from './route'
 import { getPathParams, isRouteMatchForRequest } from './path-utils'
 
-let routes: Array<Route> = []
+let routes: Route[] = []
 
-const notFound = (req: IncomingMessage, res: ServerResponse) => {
-    res.statusCode = 404
-    res.end(JSON.stringify({
-        message: 'Not Found'
-    }))
+const notFound = (req: IncomingMessage, res: ServerResponse): void => {
+  res.statusCode = 404
+  res.end(JSON.stringify({
+    message: 'Not Found'
+  }))
 }
 class App {
+  server
+  port
 
-    server
-    port
+  constructor (port: number) {
+    this.server = http.createServer()
+    this.port = port
+  }
 
-    constructor(port: Number) {
-        this.server = http.createServer()
-        this.port = port
-    }
+  addRoutes (newRoutes: Route[]): void {
+    routes = routes.concat(newRoutes)
+  }
 
-    addRoutes(newRoutes: Array<Route>) {
-        routes = routes.concat(newRoutes)
-    }
+  start (): void {
+    this.server.on('request', (req: IncomingMessage, res: ServerResponse) => {
+      console.log(`Number of connections: ${this.server.connections}`)
+      console.log(`Request: Date [${Date.now()}], URL [${req.url}], METHOD [${req.method}] `)
 
-    start() {
-        this.server.on('request', (req: IncomingMessage, res: ServerResponse) => {
-            console.log(`Number of connections: ${this.server.connections}`)
-            console.log(`Request: Date [${Date.now()}], URL [${req.url}], METHOD [${req.method}] `)
+      const reqUrl = new URL(req.url ?? '', `http://${req.headers.host}`) // TODO: Would be nice not having to set the protocol
 
-            const reqUrl = new URL(req.url || '', `http://${req.headers.host}`) // TODO: Would be nice not having to set the protocol
+      const routeMatch = routes.find((route) => isRouteMatchForRequest(route.url, reqUrl.pathname) && route.method === req.method)
 
-            const routeMatch = routes.find((route) => isRouteMatchForRequest(route.url, reqUrl.pathname) && route.method === req.method)
+      if (routeMatch != null) {
+        // getPathParams uses the same regexp as isRouteMatchForRequest.
+        // Consider a refactor when performance optimization is required
+        const params = getPathParams(routeMatch.url, reqUrl.pathname)
+        Object.assign(req, params)
+        routeMatch.requestHandler(req, res)
+        return
+      }
 
-            if (routeMatch != null) {
-                // getPathParams uses the same regexp as isRouteMatchForRequest.
-                // Consider a refactor when performance optimization is required
-                const params = getPathParams(routeMatch.url, reqUrl.pathname)
-                Object.assign(req, params)
-                routeMatch.requestHandler(req, res)
-                return
-            }
+      notFound(req, res)
+    })
 
-            notFound(req, res)
-            return
+    this.server.listen(this.port, () => {
+      console.log(`Server listening at port ${this.port}`)
+    })
+  }
 
-        })
-
-        this.server.listen(this.port, () => {
-            console.log(`Server listening at port ${this.port}`)
-        })
-    }
-
-    stop() {
-    }
+  // stop () {
+  // }
 }
 
-function createApp(port: Number = 8080) {
-    return new App(port)
+function createApp (port: number = 8080): App {
+  return new App(port)
 }
 
 export default createApp

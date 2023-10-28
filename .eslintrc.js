1d2c47cbb4f604f78918dffc97a62aaf22ff4be9
Alvaro <alvaroalonsoflor@gmail.com>
2023-10-28 14:30:28 +0100

    feat: add eslint with standard for ts

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    overrides: [
+        {
+            files: ['*.ts'],
+            extends: 'standard-with-typescript'
+        }
+    ],
+    parserOptions: {
+        project: true
+    }
+}

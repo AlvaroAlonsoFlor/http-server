f85ea0dad8249e5d9df8107340ad34047166ce5a
Alvaro <alvaroalonsoflor@gmail.com>
2023-10-28 15:20:46 +0100

    fix: lint path-utils

--- lib/path-utils.test.ts
+++ lib/path-utils.test.ts
@@ -1,64 +1,62 @@
-import { getPathParams, isRouteMatchForRequest} from "./path-utils";
+import { getPathParams, isRouteMatchForRequest } from './path-utils'
 
 describe('getPathParams', () => {
-    it('should extract parameters from a url', () => {
-        const route = 'user/:id'
-        const path = 'user/1'
+  it('should extract parameters from a url', () => {
+    const route = 'user/:id'
+    const path = 'user/1'
 
-        const params = getPathParams(route, path)
+    const params = getPathParams(route, path)
 
-        expect(params).toEqual({
-            id: '1'
-        })
+    expect(params).toEqual({
+      id: '1'
     })
+  })
 
-    it('should extract multiple parameters from a url', () => {
-        const route = 'user/:userId/form/:formId/foo/:bar'
-        const path = 'user/1/form/1234/foo/barista'
+  it('should extract multiple parameters from a url', () => {
+    const route = 'user/:userId/form/:formId/foo/:bar'
+    const path = 'user/1/form/1234/foo/barista'
 
-        const params = getPathParams(route, path)
+    const params = getPathParams(route, path)
 
-
-        expect(params).toEqual({
-            userId: '1',
-            formId: '1234',
-            bar: 'barista'
-        })
+    expect(params).toEqual({
+      userId: '1',
+      formId: '1234',
+      bar: 'barista'
     })
+  })
 
-    it('should return empty params when none provided', () => {
-        const route = 'user/something'
-        const path = 'user/somethingElse'
+  it('should return empty params when none provided', () => {
+    const route = 'user/something'
+    const path = 'user/somethingElse'
 
-        const params = getPathParams(route, path)
+    const params = getPathParams(route, path)
 
-        expect(params).toEqual({})
-    })
+    expect(params).toEqual({})
+  })
 })
 
 describe('routeMatcher', () => {
-    it('should match a correct route', () => {
-        // Given
-        const path = '/user/:id'
-        const requestPath = '/user/21'
+  it('should match a correct route', () => {
+    // Given
+    const path = '/user/:id'
+    const requestPath = '/user/21'
 
-        // When
-        const result = isRouteMatchForRequest(path, requestPath)
+    // When
+    const result = isRouteMatchForRequest(path, requestPath)
 
-        // Then
-        expect(result).toBe(true)
-    })
+    // Then
+    expect(result).toBe(true)
+  })
 
-    it('should not return a match if there is no correct route', () => {
-        // Given
-        const path = '/user/:id'
-        const requestPath = '/21'
+  it('should not return a match if there is no correct route', () => {
+    // Given
+    const path = '/user/:id'
+    const requestPath = '/21'
 
-        // When
-        const result = isRouteMatchForRequest(path, requestPath)
+    // When
+    const result = isRouteMatchForRequest(path, requestPath)
 
-        // Then
-        expect(result).toBe(false)
-
-    })
+    // Then
+    expect(result).toBe(false)
+  })
 })
--- lib/path-utils.ts
+++ lib/path-utils.ts
@@ -1,21 +1,21 @@
-import { Key, pathToRegexp } from 'path-to-regexp'
+import { type Key, pathToRegexp } from 'path-to-regexp'
 
-export function getPathParams (apiPath: string, requestPath: string) {
-    let keys: Array<Key> = []
-    const regexp = pathToRegexp(apiPath, keys)
-    const result = regexp.exec(requestPath) as RegExpExecArray
-    const params: Record<string, string> = {}
+export function getPathParams (apiPath: string, requestPath: string): Record<string, string> {
+  const keys: Key[] = []
+  const regexp = pathToRegexp(apiPath, keys)
+  const result = regexp.exec(requestPath) as RegExpExecArray
+  const params: Record<string, string> = {}
 
-    keys.forEach((key: Key, index: number) => {
-        const paramValueIndex = index + 1
-        params[key.name] = result[paramValueIndex]
-    })
-    return params
+  keys.forEach((key: Key, index: number) => {
+    const paramValueIndex = index + 1
+    params[key.name] = result[paramValueIndex]
+  })
+  return params
 }
 
 export function isRouteMatchForRequest (apiPath: string, requestPath: string): boolean {
-    const regexp = pathToRegexp(apiPath)
-    const result = regexp.exec(requestPath)
+  const regexp = pathToRegexp(apiPath)
+  const result = regexp.exec(requestPath)
 
-    return result != null
+  return result != null
 }
